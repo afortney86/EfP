5c4356e69a4ea5c3bf85c037d832c44bd6e94457
Anthony Fortney <afortney86@gmail.com>
2016-12-04 17:13:03 -0600

    Logic for finding out how many characters are in a string.

--- src/NumberOfCharacters.java
+++ src/NumberOfCharacters.java
@@ -1,5 +1,23 @@
 /**
  * Created by anthonyfortney on 12/4/16.
+ *
+ * Create a program that prompts for an input string and
+ * displays output that shows the input string and
+ * the number of characters the string contains.
  */
+
+import java.util.Scanner;
 public class NumberOfCharacters {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        int stringLength;
+
+        System.out.println("What is the input string?");
+        userInput = input.next();
+        stringLength = userInput.length();
+        System.out.println(userInput + " has " + stringLength + " characters");
+
+        input.close();
+    }
 }
