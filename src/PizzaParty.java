58e7584982732318bf062951f3ae146891deeb72
Anthony Fortney <afortney86@gmail.com>
2016-12-05 17:34:05 -0600

    spelling correction

--- src/PizzaParty.java
+++ src/PizzaParty.java
@@ -3,7 +3,7 @@
  *
  * Write a program to evenly divide pizzas. Prompt for the number of people, the number of pizzas
  * and the number of slices per pizza. Ensure the number of pieces comes out even.
- * Display the number of pieces of pizza each person should get. If there are left overs, show the number
+ * Display the number of pieces of pizza each person  should get. If there are left overs, show the number
  * of left over pieces.
  *
  */
