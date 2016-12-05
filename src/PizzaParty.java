e7e20687896111c21230b0eff92314a8ff868c6e
Anthony Fortney <afortney86@gmail.com>
2016-12-05 16:11:07 -0600

    Directions for program and variables

--- src/PizzaParty.java
+++ src/PizzaParty.java
@@ -1,5 +1,26 @@
 /**
  * Created by anthonyfortney on 12/5/16.
+ *
+ * Write a program to evenly divide pizzas. Prompt for the number of people, the number of pizzas
+ * and the number of slices per pizza. Ensure the number of pieces comes out even.
+ * Display the number of pieces of pizza each persone should get. If there are left overs, show the number
+ * of left over pieces.
+ *
  */
+
+import java.util.Scanner;
+
 public class PizzaParty {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int people;
+        int pizzas;
+        int slicesPerPizza;
+        int slicesPerPerson;
+
+
+
+
+
+    }
 }
