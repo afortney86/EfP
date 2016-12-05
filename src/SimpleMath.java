eaccd98b8755fe54316d3ec4c0a47e6bb4d3f943
Anthony Fortney <afortney86@gmail.com>
2016-12-05 13:07:05 -0600

    Finished code for SimpleMath. Tested

--- src/SimpleMath.java
+++ src/SimpleMath.java
@@ -1,5 +1,45 @@
 /**
  * Created by anthonyfortney on 12/5/16.
+ * Write a program that prompts for two numbers.
+ * Print the sum, difference, product, and quotient of those numbers.
  */
+
+import java.util.Scanner;
+
 public class SimpleMath {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int firstNumber;
+        int secondNumber;
+
+        System.out.print("What is the first number? ");
+        firstNumber = input.nextInt();
+        System.out.print("What is the second number? ");
+        secondNumber = input.nextInt();
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + addition(firstNumber, secondNumber));
+
+        System.out.println(firstNumber + " - " + secondNumber + " = " + subtraction(firstNumber, secondNumber));
+
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiply(firstNumber, secondNumber));
+
+        System.out.println(firstNumber + " / " + secondNumber + " = " + divide(firstNumber, secondNumber));
+
+    }
+    private static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+    private static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    private static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+
+
 }
