269b84511af5e31aeb00903f925b470825168b8f
Anthony Fortney <afortney86@gmail.com>
2016-12-05 11:24:46 -0600

    finished.

--- src/PrintingQuotes.java
+++ src/PrintingQuotes.java
@@ -1,5 +1,25 @@
 /**
  * Created by anthonyfortney on 12/5/16.
+ *
+ * Create a program that prompts for a quote and an author.
+ * Display the quotation and author.
  */
+
+import java.util.Scanner;
+
 public class PrintingQuotes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+
+        System.out.print("What is the quote? ");
+        String quote = input.next();
+
+
+
+        System.out.print("Who said it? ");
+        String author = input.next();
+
+        System.out.println(author + " says, " + "\""+quote+"\"");
+        input.close();
+    }
 }
