c6227b372bd7db834d84b84bcd92fd032925b4e0
Anthony Fortney <afortney86@gmail.com>
2016-12-06 08:59:54 -0600

    Logic done. Need to revisit to solve rounding

--- src/PaintCalculator.java
+++ src/PaintCalculator.java
@@ -1,5 +1,43 @@
 /**
  * Created by anthonyfortney on 12/5/16.
+ *
+ * Calculate gallon of paint needed to paint the ceiling of a room. Prompt for the length and width,
+ * and assume one gallon covers 350 square feet. Display the number of gallons needed to paint the ceiling as a whole number.
  */
+
+import java.util.Scanner;
+
 public class PaintCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int rate = 350; //1 gallon of paint covers 350 Square feet
+        int area;       // get the area divide by 350 and round up to nearest whole number
+        int length;
+        int width;
+        int gallons;
+
+        System.out.println("What is the length of the room?");
+        length = input.nextInt();
+
+        System.out.println("What is the width of tht room?");
+        width = input.nextInt();
+
+        area = findArea(length, width);
+
+        System.out.println(area);
+
+        gallons = area / rate;
+
+        System.out.println(gallons);
+
+
+
+
+
+    }
+    private static int findArea(int length, int width) {
+       return length * width;
+    }
+
 }
