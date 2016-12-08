395cb902cc53ab438030129b1aed57e471aef05a
Anthony Fortney <afortney86@gmail.com>
2016-12-08 08:34:57 -0600

    Added instructions and started exercise

---
+++ src/CurrencyConversion.java
@@ -0,0 +1,21 @@
+/**
+ * Created by anthonyfortney on 12/6/16.
+ *
+ * Write a program that converts currency. specifically, convert euros to U.S. dollars.
+ * Prompt for the amount of money in euros you have and prompt for the current exchange rate of the euro.
+ * print out the new amount in US dollars. the formula for currency conversion is.
+ *
+ * amount-to = amount-from * rate-from / rate to
+ * ^^^^^^^^^  ^^^^^^^^^^^^  ^^^^^^^^^^   ^^^^^^^^^
+ * Amount in   amount in    current       current
+ * US dollars   euros       exchange rate  exchange rate
+ *                          in euros        in US dollars.
+ */
+public class CurrencyConversion {
+    public static void main(String[] args) {
+        double euros;
+        double euroExRate;
+        double
+
+    }
+}
