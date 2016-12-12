5ac53b2e91979c2be90b459292b9e4200cdd9a67
Anthony Fortney <afortney86@gmail.com>
2016-12-12 17:31:06 -0600

    Complete tested, rounds up to nearest cent

--- src/CurrencyConversion.java
+++ src/CurrencyConversion.java
@@ -13,25 +13,25 @@
  */
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class CurrencyConversion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("###.##");
         double euros;
         double euroExRate;
-        double dollarExRate = 137.51;
+        double dollarExRate = 100;
         double dollars;
 
         System.out.println("How many euros are you exchanging?");
         euros = input.nextDouble();
-
         System.out.println("What is the exchange rate in euros?");
         euroExRate = input.nextDouble();
 
 
-
-
-
         dollars = euros * euroExRate / dollarExRate;
+
+        System.out.println(euros + " at and exchange rate of " + euroExRate + " is " + df.format(dollars) + " U.S. dollars.");
 
 
 
