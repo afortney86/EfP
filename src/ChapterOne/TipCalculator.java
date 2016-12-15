f009d771c2825bd3cc2e91f419b3330679db0521
Anthony Fortney <afortney86@gmail.com>
2016-12-15 09:05:12 -0600

    variables created

--- src/ChapterOne/TipCalculator.java
+++ src/ChapterOne/TipCalculator.java
@@ -3,5 +3,16 @@ package ChapterOne;
 /**
  * Created by anthonyfortney on 12/15/16.
  */
+import java.text.DecimalFormat;
+import java.util.Scanner;
 public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        double billAmount;
+        double tipAmount;
+        double total;
+
+    }
 }
